1e17f3e65cc69e4cf6b848c458e1998e445f5e79
1119wj <1119wj@naver.com>
2025-04-29 23:13:43 +0900

    feat: Radix UI Radio Group 추가 및 불필요한 페이지 삭제
    package.json과 package-lock.json에 Radix UI의 새로운 Radio Group 컴포넌트를 추가했습니다. 또한, 사용되지 않는 SearchPage, tPage, tPage2, tPage3 파일을 삭제하여 코드베이스를 정리하고 유지보수성을 향상시켰습니다.

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -9,15 +9,13 @@ import UserDetailPage from '@/Pages/UserDetailPage';
 import AIRecipePage from '@/Pages/AIRecipePage';
 import HomePage from '@/Pages/Home/HomePage';
 import IngredientsPage from '@/Pages/IngredientsPage';
-import { userDetail } from '@/mock';
-import SearchPage from '@/Pages/SearchPage';
-import Page from '@/Pages/tPage';
+import SearchPage from '@/Pages/SearchPage/SearchPage';
+
 import NewIngredientsPage from '@/Pages/NewIngredientsPage';
-import ScrollTransformNavbar from '@/Pages/tPage2';
 import LoginPage from '@/Pages/LoginPage';
 import RecipeSlideShowPage from '@/Pages/RecipeSlideShowPage';
 import GoogleCallback from '@/Pages/GoogleCallback';
-import Page2 from '@/Pages/tPage3';
+
 import CategoryDetailPage from '@/Pages/CategoryDetailPage';
 
 const router = createBrowserRouter([
@@ -80,14 +78,6 @@ const router = createBrowserRouter([
         element: <CategoryDetailPage />,
       },
     ],
-  },
-  {
-    path: 'test',
-    element: <Page />,
-  },
-  {
-    path: 'test2',
-    element: <Page2 />,
   },
 
   {
