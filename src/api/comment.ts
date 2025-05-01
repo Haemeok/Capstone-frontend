fcc7adf65dfa8c7bce7d9317e5a1c580ac5ac22e
1119wj <1119wj@naver.com>
2025-05-01 17:43:28 +0900

    feat: 댓글 API 기능 추가 및 정렬 파라미터 개선
    댓글 관련 API 기능을 추가하여 댓글 조회, 작성, 삭제 및 좋아요 기능을 구현하였습니다. 또한, 재료 API의 정렬 파라미터를 개선하여 일관성을 높였습니다. 이로 인해 코드의 가독성과 유지보수성이 향상되었습니다.

---
+++ src/api/comment.ts
@@ -0,0 +1,78 @@
+import { END_POINTS, PAGE_SIZE } from '@/constants/api';
+import { BaseQueryParams, PageResponse } from '@/type/query';
+import { Comment } from '@/type/comment';
+import { axiosInstance } from './axios';
+
+export type CommentsApiResponse = PageResponse<Comment>;
+
+export const getComments = async ({
+  pageParam = 0,
+  sort,
+  recipeId,
+  commentId,
+}: {
+  pageParam: number;
+  sort: string;
+  recipeId: number;
+  commentId?: number;
+}) => {
+  const apiParams: BaseQueryParams = {
+    page: pageParam,
+    size: PAGE_SIZE,
+    sort: `createdAt,${sort}`,
+  };
+
+  const END_POINT = commentId
+    ? END_POINTS.RECIPE_REPLY(recipeId, commentId)
+    : END_POINTS.RECIPE_COMMENT(recipeId);
+
+  const response = await axiosInstance.get<CommentsApiResponse>(END_POINT, {
+    params: apiParams,
+  });
+
+  return response.data;
+};
+
+export type PostCommentParams = {
+  recipeId: number;
+  commentId?: number;
+  comment: string;
+  userId: number;
+};
+
+export const postComment = async ({
+  recipeId,
+  commentId,
+  comment,
+  userId,
+}: PostCommentParams): Promise<Comment> => {
+  const END_POINT = commentId
+    ? END_POINTS.RECIPE_REPLY(recipeId, commentId)
+    : END_POINTS.RECIPE_COMMENT(recipeId);
+
+  const response = await axiosInstance.post<Comment>(END_POINT, {
+    content: comment,
+    userId,
+  });
+  return response.data;
+};
+
+export const deleteComment = async ({
+  recipeId,
+  commentId,
+}: {
+  recipeId: number;
+  commentId: number;
+}) => {
+  const END_POINT = commentId
+    ? END_POINTS.RECIPE_REPLY(recipeId, commentId)
+    : END_POINTS.RECIPE_COMMENT(recipeId);
+
+  const response = await axiosInstance.delete(END_POINT);
+  return response.data;
+};
+
+export const postCommentLike = async (commentId: number) => {
+  const response = await axiosInstance.post(END_POINTS.COMMENT_LIKE(commentId));
+  return response.data;
+};
