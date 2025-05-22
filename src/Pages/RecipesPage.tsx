a990139f1d9fee4bef1c4eb50a910bc0e1213397
1119wj <1119wj@naver.com>
2025-05-22 11:40:13 +0900

    feat: 여러 이미지 파일 삭제 및 RecipesPage 페이지네이션 로직 수정
    사용하지 않는 이미지 파일들을 삭제하였으며, RecipesPage 컴포넌트의 페이지네이션 로직을 개선하여 마지막 페이지 처리 방식을 수정하였습니다. 이를 통해 코드의 가독성을 높이고, 불필요한 리소스를 정리하여 유지보수성을 향상시켰습니다.

--- src/Pages/RecipesPage.tsx
+++ src/Pages/RecipesPage.tsx
@@ -38,7 +38,9 @@ const RecipesPage = () => {
         pageParam,
       }),
     getNextPageParam: (lastPage) =>
-      lastPage.last ? null : lastPage.number + 1,
+      lastPage.page.number === lastPage.page.totalPages - 1
+        ? null
+        : lastPage.page.number + 1,
     initialPageParam: 0,
   });
 
