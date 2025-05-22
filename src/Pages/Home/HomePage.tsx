ee2bb5e222682cdd8be836bbe87b5c16c90fc72e
1119wj <1119wj@naver.com>
2025-05-23 00:02:35 +0900

    feat: 새로운 이미지 파일 추가 및 홈 페이지 배너 수정
    다양한 새로운 이미지 파일을 추가하였으며, 홈 페이지의 배너 컴포넌트를 수정하여 AI 레시피 생성 기능을 강조하였습니다. 이를 통해 사용자 경험을 향상시키고, 레시피 관련 기능을 더욱 매력적으로 개선하였습니다.

--- src/Pages/Home/HomePage.tsx
+++ src/Pages/Home/HomePage.tsx
@@ -5,13 +5,17 @@ import HomeHeader from './HomeHeader';
 import RecipeSlide from './RecipeSlide';
 import HomeBanner from './HomeBanner';
 const HomePage = () => {
-  const { addToast, toastList } = useToastStore();
   return (
     <div className="flex flex-col items-center justify-center gap-4 bg-white p-6 text-gray-800">
       <HomeHeader />
 
       <CategoriesTabs title="카테고리" />
-      <HomeBanner />
+      <HomeBanner
+        title="AI 레시피 생성하기"
+        description="AI가 추천하는 레시피를 확인해보세요!"
+        image="/robot1.png"
+        to="/ai-recipe"
+      />
       <RecipeSlide title="추천 레시피" recipes={createdRecipes} />
       <RecipeSlide title="홈파티 레시피" recipes={createdRecipes} />
     </div>
