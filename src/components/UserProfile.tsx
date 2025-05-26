797fb00ad09b0867ea9b3aa32d6a51afa13aaf0e
1119wj <1119wj@naver.com>
2025-05-26 23:30:37 +0900

    feat: 사용자 프로필 컴포넌트 리팩토링 및 이미지 표시 방식 개선
    사용자 프로필 컴포넌트를 리팩토링하여 Avatar 컴포넌트를 UserProfileImage로 교체하였으며, 사용자 이름 표시를 Username 컴포넌트로 분리하였습니다. 이를 통해 코드의 가독성을 향상시키고, 사용자 프로필 정보를 보다 명확하게 표시할 수 있도록 개선하였습니다.

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -1,6 +1,6 @@
 import { User } from '@/type/user';
-import { Avatar } from './ui/avatar';
-
+import UserProfileImage from './UserProfileImage';
+import Username from './Username';
 interface UserProfileProps {
   user: User;
 }
@@ -9,15 +9,13 @@ const UserProfile = ({ user }: UserProfileProps) => {
   return (
     <div className="h-[80px] w-full max-w-md overflow-hidden">
       <div className="flex h-full items-center gap-2">
-        <Avatar className="h-full w-[80px] border-2">
-          <img
-            src={user.profileImage}
-            alt={user.nickname}
-            className="object-cover"
-          />
-        </Avatar>
+        <UserProfileImage
+          profileImage={user.profileImage ?? ''}
+          userId={user.id}
+          className="h-full w-[80px]"
+        />
         <div className="h-full p-2">
-          <h3 className="text-lg font-bold">{user.nickname}</h3>
+          <Username username={user.nickname} userId={user.id} />
           <p className="text-left text-sm">{user.profileContent}</p>
         </div>
       </div>
