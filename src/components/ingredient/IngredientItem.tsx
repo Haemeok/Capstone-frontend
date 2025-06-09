2ebe0f184671f64450ddf1fdf117ebffc249b690
1119wj <1119wj@naver.com>
2025-06-09 16:09:21 +0900

    feat: 사용자 정보 수정 기능 추가 및 UI 요소 개선
    사용자 정보 수정 기능을 위한 새로운 훅(usePutUserInfoMutation)을 추가하였으며, API 엔드포인트를 업데이트하여 프로필 이미지 업로드 및 사용자 정보 변경을 지원하도록 개선하였습니다. 또한, 재료 항목에서 선택된 재료 ID를 관리하는 로직을 추가하고, 버튼 스타일 및 레이아웃을 수정하여 사용자 경험을 향상시켰습니다. 이를 통해 코드의 가독성을 높이고, UI의 일관성을 강화하였습니다.

--- src/components/ingredient/IngredientItem.tsx
+++ src/components/ingredient/IngredientItem.tsx
@@ -7,9 +7,14 @@ import SuspenseImage from '../Image/SuspenseImage';
 type IngredientItemProps = {
   ingredient: IngredientItemType;
   isDeleteMode: boolean;
+  setSelectedIngredientIds: React.Dispatch<React.SetStateAction<number[]>>;
 };
 
-const IngredientItem = ({ ingredient, isDeleteMode }: IngredientItemProps) => {
+const IngredientItem = ({
+  ingredient,
+  isDeleteMode,
+  setSelectedIngredientIds,
+}: IngredientItemProps) => {
   const [isActive, setIsActive] = useState(false);
 
   return (
@@ -25,7 +30,10 @@ const IngredientItem = ({ ingredient, isDeleteMode }: IngredientItemProps) => {
       </div>
       {isDeleteMode && (
         <button
-          onClick={() => setIsActive(!isActive)}
+          onClick={() => {
+            setIsActive(!isActive);
+            setSelectedIngredientIds((prev) => [...prev, ingredient.id]);
+          }}
           className={cn(
             'absolute top-4 right-4 h-5 w-5 rounded-full border-2 border-gray-500 p-1',
             isActive ? 'bg-[#5cc570]' : '',
