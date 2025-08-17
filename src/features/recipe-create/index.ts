035508eb218d600d577f5f4e973d011b990275b5
1119wj <1119wj@naver.com>
2025-08-17 14:05:04 +0900

    docs: README.md 업데이트 및 프로젝트 소개 추가
    - 프로젝트 이름과 배포 주소를 명시하여 사용자에게 명확한 정보를 제공하였습니다.
    - AI 레시피 플랫폼의 기능과 이점을 설명하는 내용을 추가하여 사용자 이해를 돕고, 플랫폼의 목적을 강조하였습니다.
    - 기술적 과제 및 해결 과정을 상세히 설명하여 프로젝트의 신뢰성을 높였습니다.
    - 레시피 생성 및 개인화 데이터 관리 기능을 강조하여 사용자에게 유용한 정보를 제공하였습니다.
    - 개발 서버 시작 방법을 명시하여 개발자들이 쉽게 프로젝트를 실행할 수 있도록 하였습니다.

--- src/features/recipe-create/index.ts
+++ src/features/recipe-create/index.ts
@@ -2,3 +2,4 @@ export * from "./lib/prepareRecipeData";
 export { useCreateRecipeWithUpload } from "./model/hooks/useCreateRecipeWithUpload";
 export { useFinalizeRecipe } from "./model/hooks/useFinalizeRecipe";
 export * from "./model/types";
+export * from "./model/config";
