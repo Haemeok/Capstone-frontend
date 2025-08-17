29eb3fcbcf8c7e8805c5bc41715b702eb367ef34
1119wj <1119wj@naver.com>
2025-08-17 14:04:43 +0900

    feat: 레시피 생성 폼의 유효성 검사 및 타입 정의 개선
    - `@hookform/resolvers`와 `zod` 패키지를 추가하여 레시피 생성 폼의 유효성 검사를 강화하였습니다.
    - `RecipeFormValues` 타입을 `model/config.ts`에서 정의하여 일관성을 높였습니다.
    - 레시피 생성 폼의 기본값 및 유효성 검사 로직을 `config.ts`에 통합하여 코드의 가독성과 유지보수성을 향상시켰습니다.
    - `useSearchState` 훅에서 태그 코드 관련 상수를 `TAGS_BY_CODE`로 변경하여 가독성을 개선하였습니다.

--- src/features/recipe-create/model/types.ts
+++ src/features/recipe-create/model/types.ts
@@ -1,25 +1,25 @@
 import { IngredientPayload } from "@/entities/ingredient";
 
-export type RecipeFormValues = {
-  title: string;
-  dishType: string;
-  description: string;
-  cookingTime: string | number;
-  servings: string | number;
-  youtubeUrl?: string;
-  cookingTools?: string[];
-  tagNames: string[];
-  imageFile: FileList | null;
-  imageKey?: string | null;
-  ingredients: IngredientPayload[];
-  steps: Array<{
-    stepNumber: number;
-    instruction: string;
-    imageFile?: FileList | null;
-    imageKey?: string | null;
-    ingredients: IngredientPayload[];
-  }>;
-};
+// export type RecipeFormValues = {
+//   title: string;
+//   dishType: string;
+//   description: string;
+//   cookingTime: string | number;
+//   servings: string | number;
+//   youtubeUrl?: string;
+//   cookingTools?: string[];
+//   tagNames: string[];
+//   imageFile: FileList | null;
+//   imageKey?: string | null;
+//   ingredients: IngredientPayload[];
+//   steps: Array<{
+//     stepNumber: number;
+//     instruction: string;
+//     imageFile?: FileList | null;
+//     imageKey?: string | null;
+//     ingredients: IngredientPayload[];
+//   }>;
+// };
 
 export type FinalizeRecipeResponse = {
   recipeId: number;
