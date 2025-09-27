746b4bf90dcd9072f8b85131441de066f22dea02
1119wj <1119wj@naver.com>
2025-09-27 20:19:44 +0900

    feat: 카카오 및 네이버 OAuth 로그인 API 추가
    - 카카오와 네이버의 OAuth 로그인 기능을 위한 API 엔드포인트를 추가하였습니다.
    - 각 로그인 요청에서 CSRF 공격 방지를 위한 상태 검증 로직을 구현하였으며, 성공적인 인증 후 쿠키를 설정하고 리다이렉션 처리합니다.
    - 환경 변수 설정을 통해 개발 및 프로덕션 환경에 맞는 API 호출을 지원합니다.

---
+++ src/app/api/auth/login/kakao/route.ts
@@ -0,0 +1,44 @@
+import { NextResponse } from "next/server";
+
+import crypto from "crypto";
+
+export async function GET() {
+  try {
+    const state = crypto.randomBytes(16).toString("hex");
+
+    const kakaoAuthUrl = new URL("https://kauth.kakao.com/oauth/authorize");
+
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+    const kakaoClientId = process.env.KAKAO_CLIENT_ID;
+
+    if (!baseUrl || !kakaoClientId) {
+      throw new Error("환경 변수가 설정되지 않았습니다.");
+    }
+
+    kakaoAuthUrl.searchParams.append("client_id", kakaoClientId);
+    kakaoAuthUrl.searchParams.append(
+      "redirect_uri",
+      `${baseUrl}api/auth/callback/kakao`
+    );
+    kakaoAuthUrl.searchParams.append("response_type", "code");
+    kakaoAuthUrl.searchParams.append(
+      "scope",
+      "profile_nickname, account_email"
+    );
+    kakaoAuthUrl.searchParams.append("state", state);
+
+    const response = NextResponse.redirect(kakaoAuthUrl);
+
+    response.cookies.set("state", state, {
+      path: "/",
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 60 * 10,
+    });
+
+    return response;
+  } catch (error) {
+    console.error("Kakao login initiation error:", error);
+    return new NextResponse("Error initiating login", { status: 500 });
+  }
+}
