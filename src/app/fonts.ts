91f35fdb19ad5a112734aad46e0f44a3eda78497
1119wj <1119wj@naver.com>
2025-10-15 14:57:31 +0900

    refactor: 폰트 설정 통합 및 CSS 업데이트
    - `fonts.ts`에서 Pretendard 폰트를 통합하여 관리의 일관성을 높였습니다.
    - `pretendard400`와 `pretendard700`을 하나의 `pretendard`로 통합하고, Bold 스타일을 추가하여 코드 간결성을 개선했습니다.
    - `globals.css`에서 기본 sans-serif 폰트를 `pretendard`로 변경하여 전체적인 스타일 일관성을 강화했습니다.
    - `layout.tsx`에서 폰트 클래스를 업데이트하여 새로운 폰트 설정을 반영했습니다.

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,32 +1,17 @@
 import localFont from "next/font/local";
 
-export const pretendard700 = localFont({
-  src: [
-    { path: "./fonts/Pretendard-Bold.woff2", weight: "700", style: "normal" },
-  ],
-  display: "swap",
-  preload: true,
-  fallback: [
-    "system-ui",
-    "Noto Sans KR",
-    "Noto Serif KR",
-    "sans-serif",
-    "Apple SD Gothic Neo",
-    "Segoe UI",
-    "Roboto",
-  ],
-});
-
-export const pretendard400 = localFont({
+export const pretendard = localFont({
   src: [
     {
       path: "./fonts/Pretendard-Regular.woff2",
       weight: "400",
       style: "normal",
     },
+    { path: "./fonts/Pretendard-Bold.woff2", weight: "700", style: "normal" },
   ],
+  variable: "--font-pretendard",
   display: "swap",
-  preload: false,
+  preload: true,
   fallback: [
     "system-ui",
     "Noto Sans KR",
