60da26685fbaf7ed18e9aae6df855954a71931f8
1119wj <1119wj@naver.com>
2025-11-18 13:09:15 +0900

    feat: ToastProvider 및 Toast 컴포넌트 개선
    - ToastProvider에서 모바일 및 데스크탑 레이아웃을 분리하여 반응형 디자인을 개선했습니다.
    - Toast 컴포넌트에 데스크탑 전용 스타일과 아이콘을 추가하여 사용자 경험을 향상시켰습니다.
    - 코드 가독성을 높이기 위해 스타일 상수를 명확하게 구분했습니다.

--- src/app/providers/ToastProvider.tsx
+++ src/app/providers/ToastProvider.tsx
@@ -2,7 +2,9 @@
 
 import { useToastStore } from "@/widgets/Toast";
 import Toast from "@/widgets/Toast/ui/Toast";
+
 type ToastPosition = "top" | "middle" | "bottom";
+
 const ToastProvider = () => {
   const toastList = useToastStore((state) => state.toastList);
 
@@ -21,7 +23,7 @@ const ToastProvider = () => {
     <>
       <div
         id="toast-container-top"
-        className={`${baseContainerStyle} top-10 justify-start`}
+        className={`${baseContainerStyle} top-10 justify-start md:hidden`}
       >
         {topToasts.map((toast) => (
           <Toast key={toast.id} {...toast} />
@@ -30,7 +32,7 @@ const ToastProvider = () => {
 
       <div
         id="toast-container-middle"
-        className={`${baseContainerStyle} top-1/2 -translate-y-1/2 justify-center`}
+        className={`${baseContainerStyle} top-1/2 -translate-y-1/2 justify-center md:hidden`}
       >
         {middleToasts.map((toast) => (
           <Toast key={toast.id} {...toast} />
@@ -39,9 +41,18 @@ const ToastProvider = () => {
 
       <div
         id="toast-container-bottom"
-        className={`${baseContainerStyle} bottom-10 justify-end`}
+        className={`${baseContainerStyle} bottom-10 justify-end md:hidden`}
       >
         {bottomToasts.map((toast) => (
+          <Toast key={toast.id} {...toast} />
+        ))}
+      </div>
+
+      <div
+        id="toast-container-desktop"
+        className="pointer-events-none fixed top-5 right-5 z-[9999] hidden w-96 flex-col gap-3 md:flex"
+      >
+        {toastList.map((toast) => (
           <Toast key={toast.id} {...toast} />
         ))}
       </div>
