9e644ca0d25d31704acf2fbdd9ffbe31dbada08b
1119wj <1119wj@naver.com>
2026-01-12 20:19:30 +0900

    feat: RichToast 컴포넌트 추가 및 ToastProvider 개선
    - RichToast 컴포넌트를 추가하여 다양한 콘텐츠를 지원하는 알림 기능을 강화했습니다.
    - ToastProvider에서 Toast 렌더링 로직을 개선하여 RichToast와 일반 Toast를 구분하여 처리하도록 변경했습니다.
    - ToastType에 richContent, persistent, dismissible 속성을 추가하여 알림의 유연성을 높였습니다.
    - 코드의 가독성을 향상시키고, UI 요소의 일관성을 강화했습니다.

--- src/widgets/Toast/index.ts
+++ src/widgets/Toast/index.ts
@@ -1,2 +1,4 @@
 export { useToastStore } from "./model/store";
 export { default as Toast } from "./ui/Toast";
+export { RichToast } from "./ui/RichToast";
+export type { ToastType } from "./model/types";
--- src/widgets/Toast/model/types.ts
+++ src/widgets/Toast/model/types.ts
@@ -2,7 +2,25 @@ export type ToastType = {
   id: number;
   message: string;
   duration?: number;
-  variant: "success" | "error" | "warning" | "info" | "default";
+  variant:
+    | "success"
+    | "error"
+    | "warning"
+    | "info"
+    | "default"
+    | "rich-youtube";
   size?: "small" | "medium" | "large";
   position?: "top" | "middle" | "bottom";
+  persistent?: boolean;
+  dismissible?: "swipe" | "action" | "both";
+  richContent?: {
+    thumbnail?: string;
+    title?: string;
+    badgeIcon?: React.ReactNode;
+    subtitle?: string;
+  };
+  action?: {
+    label?: string;
+    onClick: () => void;
+  };
 };
