6916c24a6683e416e09e577214c2a84bd61733e4
1119wj <1119wj@naver.com>
2026-04-18 23:18:13 +0900

    feat(search-discovery): switch ContentPageGrid to horizontal Carousel

--- src/widgets/SearchDiscovery/ui/ContentPageGrid.tsx
+++ src/widgets/SearchDiscovery/ui/ContentPageGrid.tsx
@@ -1,16 +1,33 @@
 "use client";
 
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/shared/ui/shadcn/carousel";
+
 import { CONTENT_PAGES } from "@/shared/config/constants/content-pages";
 
 import ContentPageCard from "./ContentPageCard";
 
 const ContentPageGrid = () => {
   return (
-    <div className="grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4">
-      {CONTENT_PAGES.map((page) => (
-        <ContentPageCard key={page.id} page={page} />
-      ))}
-    </div>
+    <Carousel
+      opts={{ align: "start", dragFree: true, containScroll: "trimSnaps" }}
+      className="relative"
+    >
+      <CarouselContent className="-ml-3">
+        {CONTENT_PAGES.map((page) => (
+          <CarouselItem key={page.id} className="basis-auto pl-3">
+            <ContentPageCard page={page} />
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious className="left-2 hidden md:flex" />
+      <CarouselNext className="right-2 hidden md:flex" />
+    </Carousel>
   );
 };
 
