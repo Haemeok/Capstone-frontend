767f8074d43b81aabb6d46681a80e32e88fbf32a
1119wj <1119wj@naver.com>
2026-06-17 21:20:34 +0900

    test(i18n): absorb smartAppBanner no-Hangul into L2 (separate export)

--- src/shared/i18n/__tests__/dictNoHangul.test.ts
+++ src/shared/i18n/__tests__/dictNoHangul.test.ts
@@ -1,3 +1,4 @@
+import { smartAppBannerMessages } from "..";
 import { getDictionary } from "../getDictionary";
 import { findHangulInDict } from "../testing/staticScan";
 
@@ -7,4 +8,9 @@ it("T-09: ja 사전 전 namespace에 한국어가 없다", () => {
 
 it("T-09: en 사전 전 namespace에 한국어가 없다", () => {
   expect(findHangulInDict(getDictionary("en"))).toEqual([]);
+});
+
+it("T-09: smartAppBanner ja/en에 한국어가 없다", () => {
+  expect(findHangulInDict(smartAppBannerMessages.ja)).toEqual([]);
+  expect(findHangulInDict(smartAppBannerMessages.en)).toEqual([]);
 });
